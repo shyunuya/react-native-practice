be7aac2ea84d2e398f9d902e2aebe3aa7e30a3ed
Edward Shin <shyunuya@gmail.com>
2018-08-31 16:58:56 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => {
+  return <Header />;
+};
+
+AppRegistry.registerComponent('udemyRN', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+// declaration of component
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Image source={require('../img/avai-front-logo.png')} />
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: 'rgb(206, 179, 210)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 90,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+};
+
+export default Header;
